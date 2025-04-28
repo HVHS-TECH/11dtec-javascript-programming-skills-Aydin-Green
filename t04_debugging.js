0c3ba6f57e4ecfe628ade7fef470c7c5ca1d5d88
Aydin-Green <23194ag@hvhs.school.nz>
2025-04-28 22:00:19 +0000

    Task 4 day one started

--- t04_debugging.js
+++ t04_debugging.js
@@ -4,7 +4,7 @@ console.log["Task 4 - Debugging"]
 var area = length + width;
 
 // calculate the perimeter
-var perimeter = 2  ,length + 2 * width; 
+var perimeter = 2  ,length + (2 * width); 
 
 // Set up the size of the page.
 var length = 8;
@@ -19,5 +19,5 @@ if (perimeter > area){
     console.log("Yay, your area is larger, this is right for this example")
 }
 else{
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example)
+    console.log("Hmm, your perimeter is larger. It shouldn't be for this example")
 }
