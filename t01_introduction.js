c8e66715ac211356d7806ddcb601e5818489e8af
Aydin-Green <23194ag@hvhs.school.nz>
2025-04-28 21:41:12 +0000

    Apr 28, 2025, 9:41 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,5 +1,5 @@
 /********************* 
-Name of task: Task 01 By Aydin (Starting date: 29/04/2025)
+Name of task: Task 01 (Introduction) By Aydin (Starting date: 29/04/2025) Purpose: To get my school work done
 *********************/
 console.log("Running t01_introduction.js");
 
