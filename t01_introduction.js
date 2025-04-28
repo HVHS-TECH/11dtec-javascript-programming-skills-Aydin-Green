9d20f8245a5fa5aa017f3cff09372e5af5730cad
Aydin-Green <23194ag@hvhs.school.nz>
2025-04-28 21:49:24 +0000

    Apr 28, 2025, 9:49 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -5,7 +5,7 @@ By Aydin
 Starting date: 29/04/2025 
 Purpose: To get my school work done
 *********************/
-console.log("Running t01_introduction.js");
+console.log("Ben Britton is the best DTEC teacher I've ever had");
 
 //Variables
 
