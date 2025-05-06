83d314b1110870aee3356bc986ca85c2f63de5b0
Aydin-Green <23194ag@hvhs.school.nz>
2025-05-06 23:50:25 +0000

    May 6, 2025, 11:50 PM

---
+++ t05_alerts_prompts.js
@@ -0,0 +1,22 @@
+/********************* 
+Name of task: 
+Task 05 (Alerts & Prompts) 
+By Aydin 
+Starting date: 7/05/2025 
+Purpose: To get my school work done
+*********************/
+
+
+//Variables
+
+
+/*********************
+Main code
+*********************/
+
+
+
+
+/********************
+Functions
+********************/
