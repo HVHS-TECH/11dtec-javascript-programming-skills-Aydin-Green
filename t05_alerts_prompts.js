8ff232b450cf0fb50e3a93c6797ff6ada068635f
Aydin-Green <23194ag@hvhs.school.nz>
2025-05-06 23:57:04 +0000

    May 6, 2025, 11:57 PM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -8,7 +8,8 @@ Purpose: To get my school work done
 
 
 //Variables
-prompt("welcome to my program. /nWhat should I call you?");
+prompt("What should I call you?");
+
 
 /*********************
 Main code
