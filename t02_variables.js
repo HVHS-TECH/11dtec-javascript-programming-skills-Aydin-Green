02e57595b95a900be46395cd49a74958f70768eb
Aydin-Green <23194ag@hvhs.school.nz>
2025-05-09 02:47:02 +0000

    May 9, 2025, 2:47 AM

--- t02_variables.js
+++ t02_variables.js
@@ -8,8 +8,16 @@ Purpose: To get my school work done
 
 
 //Variables
-
-
+var UserName = prompt ("What is your name") ;
+console.log (UserName)
+var UserAge = prompt ("How old are you" + UserName + "?")
+console.log (UserAge)
+var UserPocketMoney = prompt ("How much pocket money do you have?")
+console.log (UserPocketMoney)
+var CurrentYear = prompt ("What year is it?")
+console.log (CurrentYear)
+alert ("Hello" +UserName+ "\nas of" +CurrentYear+ "you are" +UserAge+ "years old \nYour current pocket money is" +UserPocketMoney+ "")
+console.log ("Hello" +UserName+ "as of" +CurrentYear+ "you are" +UserAge+ "years old you have" +UserPocketMoney+ "dollars")
 /*********************
 Main code
 *********************/
