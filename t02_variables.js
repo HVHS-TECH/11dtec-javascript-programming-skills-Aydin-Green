42c8f2c436ba7507902501d2e52feb8674b08639
Aydin-Green <23194ag@hvhs.school.nz>
2025-05-09 02:32:35 +0000

    May 9, 2025, 2:32 AM

---
+++ t02_variables.js
@@ -0,0 +1,22 @@
+/********************* 
+Name of task: 
+Task 02 (Variables) 
+By Aydin 
+Starting date: 9/05/2025 
+Purpose: To get my school work done
+*********************/
+
+
+//Variables
+
+
+/*********************
+Main code
+*********************/
+
+
+
+
+/********************
+Functions
+********************/
