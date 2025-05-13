262bd9017308b6ca0293f69e187fb4a49fc236f6
Aydin-Green <23194ag@hvhs.school.nz>
2025-05-13 23:41:11 +0000

    May 13, 2025, 11:41 PM

---
+++ t06_arrays.js
@@ -0,0 +1,23 @@
+/********************* 
+Name of task: 
+Task 06 (Arrays) 
+By Aydin 
+Starting date: 14/05/2025 
+Purpose: To get my school work done
+*********************/
+
+
+//Variables
+
+
+
+/*********************
+Main code
+*********************/
+
+
+
+
+/********************
+Functions
+********************/
