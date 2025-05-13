fef03ffa1aedcd117a6eb3cfafe0b837b0db62bc
Aydin-Green <23194ag@hvhs.school.nz>
2025-05-13 23:54:47 +0000

    May 13, 2025, 11:54 PM

--- t06_arrays.js
+++ t06_arrays.js
@@ -10,7 +10,8 @@ Purpose: To get my school work done
 //Variables
 var ChocolateArray = ["You loath chocolate", "You dislike chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"]
 var ChocolateRawAnswer = prompt ("On a scale of 1-5 how much do you like chocolate")
-alert (ChocolateArray [(ChocolateRawAnswer+1)])
+var ChocolateAnswer = ChocolateRawAnswer-1
+alert (ChocolateArray [ChocolateAnswer])
 /*********************
 Main code
 *********************/
