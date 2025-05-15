96c6bf58f516eba3db0de3e259934f31e9c95721
Aydin-Green <23194ag@hvhs.school.nz>
2025-05-15 01:08:38 +0000

    May 15, 2025, 1:08 AM

--- t07_if.js
+++ t07_if.js
@@ -8,10 +8,7 @@ Purpose: To get my school work done
 
 
 //Variables
-var ChocolateArray = ["You loath chocolate", "You dislike chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"]
-var ChocolateRawAnswer = prompt ("On a scale of 1-5 how much do you like chocolate")
-var ChocolateAnswer = ChocolateRawAnswer-1
-alert (ChocolateArray [ChocolateAnswer])
+
 /*********************
 Main code
 *********************/
