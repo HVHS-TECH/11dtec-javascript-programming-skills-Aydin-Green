b64d8c9404071e5ae8d7239d3415b6e91f014f0d
Aydin-Green <23194ag@hvhs.school.nz>
2025-05-15 01:02:37 +0000

    May 15, 2025, 1:02 AM

---
+++ t07_if.js
@@ -0,0 +1,24 @@
+/********************* 
+Name of task: 
+Task 07 (If) 
+By Aydin 
+Starting date: 15/05/2025 
+Purpose: To get my school work done
+*********************/
+
+
+//Variables
+var ChocolateArray = ["You loath chocolate", "You dislike chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"]
+var ChocolateRawAnswer = prompt ("On a scale of 1-5 how much do you like chocolate")
+var ChocolateAnswer = ChocolateRawAnswer-1
+alert (ChocolateArray [ChocolateAnswer])
+/*********************
+Main code
+*********************/
+
+ 
+
+
+/********************
+Functions
+********************/
