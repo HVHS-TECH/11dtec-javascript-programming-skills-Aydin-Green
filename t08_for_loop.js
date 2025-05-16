16abb8c00851159b4dd7b07ba8665e5fe292135a
Aydin-Green <23194ag@hvhs.school.nz>
2025-05-16 02:51:51 +0000

    May 16, 2025, 2:51 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -8,7 +8,12 @@ Purpose: To get my school work done
 
 
 //Variables
-
+var NumberOne = prompt ("pick a random number")
+var NumberTwo = prompt ("pick a random number")
+var NumberThree = prompt ("pick a random number")
+var NumberFour = prompt ("pick a random number")
+var NumberFive = prompt ("pick a random number")
+alert ("The sum of your numbers is" +(NumberOne+NumberTwo+NumberThree+NumberFour+NumberFive)+ "")
 
 /*********************
 Main code
