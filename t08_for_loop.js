27b8d2e0e16113e534c90431393cb2be773a0039
Aydin-Green <23194ag@hvhs.school.nz>
2025-05-16 02:41:12 +0000

    May 16, 2025, 2:41 AM

---
+++ t08_for_loop.js
@@ -0,0 +1,22 @@
+/********************* 
+Name of task: 
+Task 08 (For Loop) 
+By Aydin 
+Starting date: 16/05/2025 
+Purpose: To get my school work done
+*********************/
+
+
+//Variables
+
+
+/*********************
+Main code
+*********************/
+
+
+
+
+/********************
+Functions
+********************/
