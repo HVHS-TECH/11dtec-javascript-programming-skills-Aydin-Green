6964ad0ab9fa52aa9a6f80876a9b86822e987bb6
Aydin-Green <23194ag@hvhs.school.nz>
2025-06-03 22:27:22 +0000

    Jun 3, 2025, 10:27 PM

--- t09_functions.js
+++ t09_functions.js
@@ -12,8 +12,8 @@ GetUserName()
 GetUserAge()
 GetUserPocketMoney()
 GetCurrentYear()
-alert ("Hello " +UserName+ " \nAs of " +CurrentYear+ " you are " +UserAge+ " years old \nYou were born in " +(CurrentYear-UserAge)+ "\nIn 10 years you'll be " +(10+UserAge)+ "\nYour current pocket money is " +UserPocketMoney+ "\nHalf of your money is " +(UserPocketMoney/2))
-console.log ("Hello " +UserName+ " As of " +CurrentYear+ " you are " +UserAge+ " years old You were born in " +(CurrentYear-UserAge)+ " In 10 years you'll be " +(10+UserAge)+  " you have " +UserPocketMoney+ " dollars Half of your money is " +(UserPocketMoney/2))
+alert("Hello " + UserName + " \nAs of " + CurrentYear + " you are " + UserAge + " years old \nYou were born in " + (CurrentYear - UserAge) + "\nIn 10 years you'll be " + (10 + UserAge) + "\nYour current pocket money is " + UserPocketMoney + "\nHalf of your money is " + (UserPocketMoney / 2))
+console.log("Hello " + UserName + " As of " + CurrentYear + " you are " + UserAge + " years old You were born in " + (CurrentYear - UserAge) + " In 10 years you'll be " + (10 + UserAge) + " you have " + UserPocketMoney + " dollars Half of your money is " + (UserPocketMoney / 2))
 /*********************
 Main code
 *********************/
@@ -26,21 +26,21 @@ Functions
 ********************/
 
 function GetUserName() {
-    var UserName = prompt ("What is your name") ;
-console.log (UserName)
+    var UserName = prompt("What is your name");
+    console.log(UserName)
 }
 
 function GetUserAge() {
-    var UserAge = prompt ("How old are you?")
-console.log (UserAge)
+    var UserAge = prompt("How old are you?")
+    console.log(UserAge)
 }
 
 function GetUserPocketMoney() {
-    var UserPocketMoney = prompt ("How much pocket money do you have?")
-console.log (UserPocketMoney)
+    var UserPocketMoney = prompt("How much pocket money do you have?")
+    console.log(UserPocketMoney)
 }
 
 function GetCurrentYear() {
-    var CurrentYear = prompt ("What year is it?")
-console.log (CurrentYear)
+    var CurrentYear = prompt("What year is it?")
+    console.log(CurrentYear)
 }
