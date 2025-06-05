79a9c9748ac1ce045530a3bdd92aecafcd69761c
Aydin-Green <23194ag@hvhs.school.nz>
2025-06-05 00:49:32 +0000

    Task 10 actually done

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -5,15 +5,10 @@ By Aydin
 Starting date: 4/06/2025 
 Purpose: To get my school work done
 *********************/
-var UserName,UserAge,UserPocketMoney,CurrentYear = "test";
+var UserName,UserAge,UserPocketMoney,CurrentYear = "Null";
 
 //Variables
-GetUserName()
-GetUserAge()
-GetUserPocketMoney()
-GetCurrentYear()
-alert(`Hello ${UserName}\nAs of ${CurrentYear} you are ${UserAge} years old\nYou were born in ${(CurrentYear - UserAge)} In 10 years you'll be ${(10 + UserAge)}\nYour current pocket money is ${UserPocketMoney}\nHalf of your money is ${(UserPocketMoney / 2)}`)
-console.log(`Hello ${UserName}\nAs of ${CurrentYear} you are ${UserAge} years old\nYou were born in ${(CurrentYear - UserAge)}In 10 years you'll be ${(10 + UserAge)}\nYour current pocket money is ${UserPocketMoney}\nHalf of your money is ${(UserPocketMoney / 2)}`)
+
 /*********************
 Main code
 *********************/
@@ -43,4 +38,13 @@ function GetUserPocketMoney() {
 function GetCurrentYear() {
     CurrentYear = prompt("What year is it?")
     console.log(CurrentYear)
+}
+
+function start() {
+GetUserName()
+GetUserAge()
+GetUserPocketMoney()
+GetCurrentYear()
+alert(`Hello ${UserName}\nAs of ${CurrentYear} you are ${UserAge} years old\nYou were born in ${(CurrentYear - UserAge)} In 10 years you'll be ${(10 + UserAge)}\nYour current pocket money is ${UserPocketMoney}\nHalf of your money is ${(UserPocketMoney / 2)}`)
+console.log(`Hello ${UserName}\nAs of ${CurrentYear} you are ${UserAge} years old\nYou were born in ${(CurrentYear - UserAge)}In 10 years you'll be ${(10 + UserAge)}\nYour current pocket money is ${UserPocketMoney}\nHalf of your money is ${(UserPocketMoney / 2)}`)
 }
