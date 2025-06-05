afa908e54c8df7e4f3cf938ffb2828cc24c03b61
Aydin-Green <23194ag@hvhs.school.nz>
2025-06-05 00:36:13 +0000

    Task 10 done

---
+++ t10_activate_via_button.js
@@ -0,0 +1,46 @@
+/********************* 
+Name of task: 
+Task 10 (Activate Via Button) 
+By Aydin 
+Starting date: 4/06/2025 
+Purpose: To get my school work done
+*********************/
+var UserName,UserAge,UserPocketMoney,CurrentYear = "test";
+
+//Variables
+GetUserName()
+GetUserAge()
+GetUserPocketMoney()
+GetCurrentYear()
+alert(`Hello ${UserName}\nAs of ${CurrentYear} you are ${UserAge} years old\nYou were born in ${(CurrentYear - UserAge)} In 10 years you'll be ${(10 + UserAge)}\nYour current pocket money is ${UserPocketMoney}\nHalf of your money is ${(UserPocketMoney / 2)}`)
+console.log(`Hello ${UserName}\nAs of ${CurrentYear} you are ${UserAge} years old\nYou were born in ${(CurrentYear - UserAge)}In 10 years you'll be ${(10 + UserAge)}\nYour current pocket money is ${UserPocketMoney}\nHalf of your money is ${(UserPocketMoney / 2)}`)
+/*********************
+Main code
+*********************/
+
+
+
+
+/********************
+Functions
+********************/
+
+function GetUserName() {
+    UserName = prompt("What is your name");
+    console.log(UserName)
+}
+
+function GetUserAge() {
+    UserAge = prompt("How old are you?")
+    console.log(UserAge)
+}
+
+function GetUserPocketMoney() {
+    UserPocketMoney = prompt("How much pocket money do you have?")
+    console.log(UserPocketMoney)
+}
+
+function GetCurrentYear() {
+    CurrentYear = prompt("What year is it?")
+    console.log(CurrentYear)
+}
